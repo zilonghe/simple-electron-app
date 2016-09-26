f02289fa866a13248efe06c31126147b10105eff
Ryan Baer <ryan@techmatehq.com>
2016-09-26 00:08:19 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const {ipcRenderer} = require('electron')
+
+document.addEventListener('DOMContentLoaded', () => {
+  let n = new Notification('You did it!', {
+    body: 'Nice work.'
+  })
+
+  // Tell the notification to show the menubar popup window on click
+  n.onclick = () => { ipcRenderer.send('show-window') }
+
+})
